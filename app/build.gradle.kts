b85dde5cb2274ae2049f0de3a4d5bb43fe660323
Omarf551 <of663962@gmail.com>
2023-12-01 20:48:17 -0700

    Se termino el proyecto de dados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.nobby.myapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.nobby.myapplication"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/nobby/myapplication/MainActivity.kt
+++ app/src/main/java/com/nobby/myapplication/MainActivity.kt
@@ -2,10 +2,42 @@ package com.nobby.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val etDados = findViewById<EditText>(R.id.etDados)
+        val etLados = findViewById<EditText>(R.id.etLados)
+        val btnTirar = findViewById<Button>(R.id.tirard)
+        val tvTotal = findViewById<TextView>(R.id.tvTotal)
+
+        btnTirar.setOnClickListener {
+            val numDados = etDados.text.toString().toIntOrNull()
+            val numLados = etLados.text.toString().toIntOrNull()
+
+            if (numDados != null && numLados != null && numDados > 0 && numLados > 0) {
+                val resultadoTotal = tirarDados(numDados, numLados)
+                tvTotal.text = "Total: $resultadoTotal"
+            } else {
+                tvTotal.text = "faltan datos"
+            }
+        }
+
+
     }
+
+    private fun tirarDados(numDados: Int, numLados: Int): Int {
+        var total = 0
+        for (i in 1..numDados) {
+            total += Random.nextInt(1, numLados + 1)
+        }
+        return total
+    }
+    
+    
 }
